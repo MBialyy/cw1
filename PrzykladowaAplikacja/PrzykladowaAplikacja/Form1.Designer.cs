42b16d9161ff6af27ebff86b3371aa347208d20c
Miłosz Białecki <mbialy97@gmail.com>
2022-03-19 14:09:09 +0100

    Dodane pliki

---
+++ PrzykladowaAplikacja/PrzykladowaAplikacja/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace PrzykladowaAplikacja
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.edycjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.edycjaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.imieTextBox = new System.Windows.Forms.TextBox();
+            this.NazwiskoTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.studenciListBox = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.edycjaToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(752, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.edycjaToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // edycjaToolStripMenuItem
+            // 
+            this.edycjaToolStripMenuItem.Name = "edycjaToolStripMenuItem";
+            this.edycjaToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.edycjaToolStripMenuItem.Text = "Zakończ";
+            // 
+            // edycjaToolStripMenuItem1
+            // 
+            this.edycjaToolStripMenuItem1.Name = "edycjaToolStripMenuItem1";
+            this.edycjaToolStripMenuItem1.Size = new System.Drawing.Size(53, 20);
+            this.edycjaToolStripMenuItem1.Text = "Edycja";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(12, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Wyświetl Info";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(64, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Imię";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(24, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nazwisko";
+            // 
+            // imieTextBox
+            // 
+            this.imieTextBox.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.imieTextBox.Location = new System.Drawing.Point(152, 93);
+            this.imieTextBox.Name = "imieTextBox";
+            this.imieTextBox.Size = new System.Drawing.Size(215, 26);
+            this.imieTextBox.TabIndex = 4;
+            // 
+            // NazwiskoTextBox
+            // 
+            this.NazwiskoTextBox.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NazwiskoTextBox.Location = new System.Drawing.Point(152, 135);
+            this.NazwiskoTextBox.Name = "NazwiskoTextBox";
+            this.NazwiskoTextBox.Size = new System.Drawing.Size(215, 26);
+            this.NazwiskoTextBox.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(239, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 37);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Dodaj studenta";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // studenciListBox
+            // 
+            this.studenciListBox.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.studenciListBox.FormattingEnabled = true;
+            this.studenciListBox.ItemHeight = 18;
+            this.studenciListBox.Location = new System.Drawing.Point(442, 96);
+            this.studenciListBox.Name = "studenciListBox";
+            this.studenciListBox.Size = new System.Drawing.Size(231, 184);
+            this.studenciListBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(442, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Lista studentów";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(752, 318);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.studenciListBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.NazwiskoTextBox);
+            this.Controls.Add(this.imieTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Lista studentów";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem plikToolStripMenuItem;
+        private ToolStripMenuItem edycjaToolStripMenuItem;
+        private ToolStripMenuItem edycjaToolStripMenuItem1;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox imieTextBox;
+        private TextBox NazwiskoTextBox;
+        private Button button2;
+        private ListBox studenciListBox;
+        private Label label3;
+    }
+}
---
+++ PrzykladowaAplikacja/PrzykladowaAplikacja/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace PrzykladowaAplikacja.Models
+{
+    public class Student
+    {
+        //Dane
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+        public string ImieINazwisko { 
+            get
+            {
+                return Imie + " " + Nazwisko;
+            }
+        }
+    }
+}
